b6c6346ad5aa754d983da93332142cbc27def288
Kevin Kener <kevink.contacto@gmail.com>
2024-02-17 21:09:13 -0300

    add Header Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Box, Container, HStack, VStack, Flex, Text, Link } from '@chakra-ui/react'
-export default function Header() {
+export default function Header({bg}) {
+
     return (
-        <Box w={'full'} h={'80px'} bg={'brown'}>
-            <Container maxW={'6xl'} h={'inherit'}>
+        <Box as='header' w={'full'} h={'80px'} bg={bg} pos={'fixed'} top={0}>
+            <Container as={'nav'} maxW={'6xl'} h={'inherit'}>
                 <HStack h={'inherit'} justifyContent={'space-between'} color={'white'}>
                     <Text fontSize={'2xl'}>UI</Text>
 
