8a24f3595c279631e0b8c1dffbd419ab3eeea30d
Kevin Kener <kevink.contacto@gmail.com>
2024-02-17 21:09:24 -0300

    add Scroll event

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from "react"
 import Header from "./components/Header/Header"
+import Hero from "./components/Hero/Hero"
+import Seccion from "./components/Seccion"
+import { Box } from "@chakra-ui/react"
 function App() {
+  
+  const [scrollPosition, setScrollPosition] = React.useState('blackAlpha.400');
+  const handleScroll = (e) => {
+    const { scrollTop, scrollHeight, clientHeight } = e.target;
+    const position = Math.ceil(
+        (scrollTop / (scrollHeight - clientHeight)) * 100
+    );
+    setScrollPosition(position > 5 ? "blackAlpha.400" : "transparent" )
+};
 
   return (
-  <>
-    <Header/>
-  </>
+    <Box as='div' h={'100vh'} id="page" 
+    overflow={'auto'}
+    // onWheel={e => console.log(window.scrollY)} 
+    onScroll={handleScroll}>
+      <Header bg={scrollPosition}/>
+      <Hero/>
+      <Seccion>aa</Seccion>
+    </Box>
   ) 
 }
 
