56e98787435b82176f3c970236f0a2942ec7e053
Kevin Kener <kevink.contacto@gmail.com>
2024-02-17 21:08:58 -0300

    add Footer Component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Container, HStack, VStack, Flex, Text, Link } from '@chakra-ui/react'
 
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <Box w={'full'} h={'80px'} bg={'brown'}>
+            <Container maxW={'6xl'} h={'inherit'}>
+                <HStack h={'inherit'} justifyContent={'space-between'} color={'white'}>
+                    <Text fontSize={'2xl'}>FOOTER</Text>
+
+                    <HStack>
+                        <Link>Inicio</Link>
+                        <Link>Nosotros</Link>
+                        <Link>Servicios</Link>
+                        <Link>Contacto</Link>
+                    </HStack>
+                </HStack>
+            </Container>
+        </Box>
+    )
 }
